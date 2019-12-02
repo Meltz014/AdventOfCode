5f324e701503beb098462df90ed4b782705ede61
David Melton <dmeltz014@gmail.com>
2019-12-02 09:30:15 -0700

    part 1 & 2 for day 1

--- day1/solve.py
+++ day1/solve.py
@@ -1,9 +1,25 @@
 
 import numpy
 
+def find_fuel(masses):
+    fuel = masses // 3 - 2
+    fuel[fuel < 0] = 0
+    if numpy.sum(fuel) == 0:
+        return fuel
+    new_f = fuel + find_fuel(fuel)
+    return new_f
+
+def part1(masses):
+    fuel = (masses // 3) - 2
+    print(f'Part 1: {numpy.sum(fuel)}')
+
+def part2(masses):
+    full = find_fuel(masses)
+    print(f'Part 2: {numpy.sum(full)}')
+
 def main():
     masses = numpy.loadtxt('input.txt', dtype=numpy.uint32)
-    fuel = (masses // 3) - 2
-    print(numpy.sum(fuel))
+    part1(masses)
+    part2(masses)
 
 main()
