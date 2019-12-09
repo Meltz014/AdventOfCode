acea1ae21b305c712b8853c7692800c785006e0b
David Melton <dmeltz014@gmail.com>
2019-12-09 09:56:06 -0700

    day 9

---
+++ day9/solve.py
@@ -0,0 +1,38 @@
+import numpy
+from queue import Empty
+from intcode import CPU
+
+def main():
+    # part 1
+    memory_ = numpy.loadtxt('day9\input.txt', delimiter=',', dtype=numpy.int64)
+    #memory_ = numpy.fromstring('1102,34915192,34915192,7,4,7,99,0', sep=',', dtype=numpy.int64)
+    #memory_ = numpy.fromstring('104,1125899906842624,99', sep=',', dtype=numpy.int64)
+    #memory_ = numpy.fromstring('109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99', sep=',', dtype=numpy.int64)
+
+    ### Part 1
+    memory = numpy.array(memory_, copy=True)
+    cpu = CPU(memory)
+    cpu.queue_input(1)
+    cpu.exec()
+    print(f'Part 1 output:')
+    try:
+        while True:
+            print(str(cpu.get_output()) + ' ',)
+    except Empty:
+        print('done')
+
+    ### Part 2
+    memory = numpy.array(memory_, copy=True)
+    cpu = CPU(memory)
+    cpu.queue_input(2)
+    cpu.exec()
+    print(f'Part 2 output:')
+    try:
+        while True:
+            print(str(cpu.get_output()) + ' ',)
+    except Empty:
+        print('done')
+
+
+if __name__ == '__main__':
+    main()
--- intcode.py
+++ intcode.py
@@ -1,4 +1,5 @@
 import queue
+import numpy
 
 debug = False
 # opcodes
@@ -11,10 +12,14 @@ JE = 5 # jump if true
 JNE = 6 # jump if false
 LT = 7 # less than
 EQ = 8 # equal
+RB = 9 # relative base mod
 
 # Param modes
 POS = 0
 IMM = 1
+REL = 2
+
+REALLOC_BUF = 1000
 
 def get_modes(modes_int, n):
     modes = []
@@ -28,6 +33,7 @@ class CPU():
     def __init__(self, memory, input_block=False, input_q=None):
         self.memory = memory
         self.pc = 0
+        self.relative_base = 0
 
         self.handlers = {
             ADD: self.handle_add,
@@ -38,6 +44,7 @@ class CPU():
             JNE: self.handle_jne,
             LT: self.handle_lt,
             EQ: self.handle_eq,
+            RB: self.handle_rb,
         }
 
         self.input_block = input_block
@@ -57,6 +64,8 @@ class CPU():
         self.pc = pc
         while True:
             opcode_mode = self.memory[self.pc]
+            if opcode_mode == 203:
+                print('break')
             opcode = opcode_mode % 100
             modes = opcode_mode // 100
             if opcode == HALT:
@@ -70,45 +79,69 @@ class CPU():
             else:
                 raise(Exception(f'Invalid opcode {opcode} at position {self.pc}'))
 
+    def _memspace_guard(self, addr):
+        if addr >= len(self.memory):
+            if (addr - len(self.memory)) > REALLOC_BUF:
+                size = addr - len(self.memory) + REALLOC_BUF
+            else:
+                size = REALLOC_BUF
+            self.memory = numpy.append(self.memory, numpy.zeros(size, dtype=self.memory.dtype))
+
+    def read(self, addr):
+        self._memspace_guard(addr)
+        return self.memory[addr]
+
+    def write(self, addr, newval):
+        self._memspace_guard(addr)
+        self.memory[addr] = newval
+
     def get_param_vals(self, modes_int, n):
         vals = []
         for (i, mode) in enumerate(get_modes(modes_int, n)):
-            param = self.memory[self.pc+1+i]
+            param = self.read(self.pc+1+i)
             if mode == POS:
-                vals.append(self.memory[param])
+                vals.append(self.read(param))
             elif mode == IMM:
                 vals.append(param)
+            elif mode == REL:
+                vals.append(self.read(self.relative_base + param))
 
         return vals
 
     def handle_add(self, modes):
         (_, _, out_mode) = get_modes(modes, 3)
-        assert out_mode == POS
+        assert out_mode != IMM
         (left, right) = self.get_param_vals(modes, 2)
-        out = self.memory[self.pc+3]
+        out = self.read(self.pc+3)
+        if out_mode == REL:
+            out = out + self.relative_base
         if debug:
             print(f'Add: {left}, {right}')
-        self.memory[out] = left + right
+        self.write(out, left + right)
         if debug:
-            print(f'Res {out}: {self.memory[out]}')
+            print(f'Res {out}: {self.read(out)}')
         self.pc += 4
 
     def handle_mul(self, modes):
         (_, _, out_mode) = get_modes(modes, 3)
-        assert out_mode == POS
+        assert out_mode != IMM
         (left, right) = self.get_param_vals(modes, 2)
-        out = self.memory[self.pc+3]
+        out = self.read(self.pc+3)
+        if out_mode == REL:
+            out = out + self.relative_base
         if debug:
             print(f'Mul: {left}, {right}')
-        self.memory[out] = left * right
+        self.write(out, left * right)
         if debug:
-            print(f'Res {out}: {self.memory[out]}')
+            print(f'Res {out}: {self.read(out)}')
         self.pc += 4
 
     def handle_input(self, modes):
         (in_mode, ) = get_modes(modes, 1)
-        assert in_mode == POS
-        out = self.memory[self.pc+1]
+        assert in_mode != IMM
+        out = self.read(self.pc+1)
+        if in_mode == REL:
+            out = out + self.relative_base
         try:
             if self.input_block:
                 user = self.input_q.get()
@@ -116,7 +149,7 @@ class CPU():
                 user = self.input_q.get_nowait()
         except queue.Empty:
             user = int(input('Input an int:'))
-        self.memory[out] = user
+        self.write(out, user)
         self.pc += 2
 
     def handle_output(self, modes):
@@ -141,16 +174,25 @@ class CPU():
 
     def handle_lt(self, modes):
         (_, _, out_mode) = get_modes(modes, 3)
-        assert out_mode == POS
+        assert out_mode != IMM
         (left, right) = self.get_param_vals(modes, 2)
-        out = self.memory[self.pc+3]
-        self.memory[out] = left < right
+        out = self.read(self.pc+3)
+        if out_mode == REL:
+            out = out + self.relative_base
+        self.write(out, left < right)
         self.pc += 4
 
     def handle_eq(self, modes):
         (_, _, out_mode) = get_modes(modes, 3)
-        assert out_mode == POS
+        assert out_mode != IMM
         (left, right) = self.get_param_vals(modes, 2)
-        out = self.memory[self.pc+3]
-        self.memory[out] = left == right
+        out = self.read(self.pc+3)
+        if out_mode == REL:
+            out = out + self.relative_base
+        self.write(out, left == right)
         self.pc += 4
+
+    def handle_rb(self, modes):
+        (new_rb, ) = self.get_param_vals(modes, 1)
+        self.relative_base += new_rb
+        self.pc += 2
