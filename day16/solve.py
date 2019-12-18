63852259c381e6f4f39716d64e21d7d6a3d2a6bc
David Melton <dmeltz014@gmail.com>
2019-12-18 10:21:09 -0700

    day 16 final

--- day16/solve.py
+++ day16/solve.py
@@ -1,6 +1,7 @@
 import numpy
 from collections import defaultdict
 import math
+import time
 
 patterns = {}
 base = [0, 1, 0, -1]
@@ -27,23 +28,28 @@ def main():
         signal_str = fid.read().strip()
     signal = [int(i) for i in signal_str]
     new_signal = numpy.array(signal, dtype=numpy.int8)
-    reps = 1
+    reps = 100
     for i in range(reps):
         new_signal = numpy.array([phase_digit(new_signal, i+1) for i in range(len(new_signal))], dtype=numpy.int8)
 
-    print('Part 1:' + ''.join(str(s) for s in new_signal[:8]))
+    print('Part 1: ' + ''.join(str(s) for s in new_signal[:8]))
+    #print(''.join(str(s) for s in new_signal))
 
     #### part 2
     real_ = numpy.array(signal, dtype=numpy.int8)
     offset = int(''.join(str(i) for i in real_[:7]))
     real_signal = numpy.tile(real_, 10000)
-
-    patterns = {}
+    offset_signal = real_signal[offset:]
+    offset_signal = offset_signal[::-1] # backwards for cumsum
+    start_time = time.time()
     for i in range(reps):
-        real_signal = numpy.array([phase_digit(real_signal, i+1) for i in range(len(real_signal))], dtype=numpy.int8)
+        offset_signal = numpy.mod( numpy.cumsum(offset_signal), 10 )
 
-    print('Part 2:' + ''.join(str(i) for i in real_signal[offset:offset+8]))
+    # re-reverse it
+    offset_signal = offset_signal[::-1]
 
+    print('Part 2: ' + ''.join(str(s) for s in offset_signal[:8]))
+    #print(''.join(str(s) for s in offset_signal))
 
 if __name__ == '__main__':
     main()
