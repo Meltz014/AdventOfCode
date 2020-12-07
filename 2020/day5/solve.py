9ab548b7a2adfeae3ff50c01284dfe10be8bc1aa
David Melton <dmeltz014@gmail.com>
2020-12-07 10:52:39 -0700

    day 5 opti

--- 2020/day5/solve.py
+++ 2020/day5/solve.py
@@ -2,14 +2,13 @@
 import numpy
 
 def part1(seats):
-    target = max(seats, key=lambda x: x[2])
-    print(f'Part 1: {target[2]}')
+    print(f'Part 1: {max(seats)}')
 
 def part2(seats):
-    max_id = max(seats, key=lambda x: x[2])[2]
-    min_id = min(seats, key=lambda x: x[2])[2]
+    max_id = max(seats)
+    min_id = min(seats)
 
-    existing = set([seat[2] for seat in seats])
+    existing = set(seats)
     missing = set(range(min_id, max_id+1)) - existing
     print(f'Part 2: {missing}')
 
@@ -20,11 +19,9 @@ def main():
     seats = [] # (row, col, id)
     for line in data:
         # F is 0, B is 1
-        row_bin = line[:7].replace('F', '0').replace('B', '1')
         # L is 0, R is 1
-        col_bin = line[7:10].replace('L', '0').replace('R', '1')
-        (row, col) = (int(row_bin, 2), int(col_bin, 2))
-        seats.append((row, col, row*8 + col))
+        id_bin = line[:10].replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')
+        seats.append(int(id_bin, 2))
 
     part1(seats)
     part2(seats)
