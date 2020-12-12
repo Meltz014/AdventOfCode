f801b7e4e3cbfd11ec7387881716227fe6421710
David Melton <dmeltz014@gmail.com>
2020-12-11 20:42:47 -0700

    Add generic runner class

---
+++ AoC.py
@@ -0,0 +1,39 @@
+import numpy
+
+class NotImplementedError(Exception):
+    pass
+
+class AoC():
+
+    example_data = ""
+
+    def __init__(self, day, use_example=False):
+        self._day = day
+        self._use_example = use_example
+
+    def read_input_txt(self):
+        if self._use_example:
+            if self.example_data:
+                # add \n to replicate behavior of fid.readlines()
+                return [l + '\n' for l in self.example_data.splitlines()]
+            else:
+                print('warning: no example data defined.  Using input.txt')
+        with open(f'day{self._day}\input.txt') as fid:
+            return fid.readlines()
+
+    def read_input_numeric(self, dtype=numpy.int64):
+        if self._use_example:
+            if self.example_data:
+                return numpy.fromstring(f'day{self._day}\input.txt', dtype=dtype, sep='\n')
+            else:
+                print('warning: no example data defined.  Using input.txt')
+        return numpy.loadtxt(f'day{self._day}\input.txt', dtype=dtype)
+
+    def parse(self):
+        raise NotImplementedError()
+
+    def part1(self):
+        raise NotImplementedError()
+
+    def part2(self):
+        raise NotImplementedError()
--- runner.py
+++ runner.py
@@ -6,6 +6,7 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', required=True)
 parser.add_argument('-y', required=True)
+parser.add_argument('-e', action='store_true', required=False)
 parser.add_argument('-debug', action='store_true', required=False)
 
 args = parser.parse_args()
@@ -24,8 +25,30 @@ os.chdir(f'y{year}')
 solve = importlib.import_module(f'y{year}.day{day}.solve')
 
 print(f'running day{day}')
-start = time.time()
-solve.main()
-end = time.time()
+if hasattr(solve, 'Solver'):
+    # new Solver class
+    solver = solve.Solver(day, use_example=args.e)
+    parse_start = time.time()
+    solver.parse()
+    parse_end = time.time()
+    p1_start = time.time()
+    p1 = solver.part1()
+    p1_end = time.time()
+    print(f'Part 1: {p1}')
+    p2_start = time.time()
+    p2 = solver.part2()
+    p2_end = time.time()
+    print(f'Part 2: {p2}')
 
-print(f'Elapsed: {end-start}s')
+    print(f'Elapsed (parse): {parse_end-parse_start}')
+    print(f'Elapsed (part1): {p1_end-p1_start}')
+    print(f'Elapsed (part2): {p2_end-p2_start}')
+
+
+else:
+    # legacy main method
+    start = time.time()
+    solve.main()
+    end = time.time()
+
+    print(f'Elapsed: {end-start}s')
