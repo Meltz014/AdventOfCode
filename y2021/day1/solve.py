5c3493d6f98b896359a62dd390eedf734de5212c
David Melton <dmelton@qti.qualcomm.com>
2021-11-30 22:32:40 -0700

    2021d1

---
+++ y2021/day1/solve.py
@@ -0,0 +1,25 @@
+from AoC import AoC
+import numpy as np
+
+class Solver(AoC):
+    example_data = """199
+200
+208
+210
+200
+207
+240
+269
+260
+263"""
+
+    def parse(self):
+        self.readings = self.read_input_numeric()
+
+    def part1(self):
+        return np.count_nonzero(np.diff(self.readings) > 0)
+        
+
+    def part2(self):
+        conv = np.convolve(self.readings, np.ones(3, dtype=int), 'valid')
+        return np.count_nonzero(np.diff(conv) > 0)
